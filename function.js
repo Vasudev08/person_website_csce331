aa575be748e3da40f38318922cc0e96f74c28678
Vasudev08 <vasu14devagarwal@gmail.com>
2023-09-14 22:28:32 -0500

    updated the 2nd style css file

--- function.js
+++ function.js
@@ -1,38 +1,37 @@
 // Function to toggle the theme
 function toggleTheme() {
-  var themeLink = document.getElementById("theme-link");
-
-  // Check the current theme preference in local storage
-  var currentTheme = localStorage.getItem("theme") || "light";
-
-  // Toggle the theme by changing the href attribute of the link element
-  if (currentTheme === "light") {
-    themeLink.href = "style.css";
-    localStorage.setItem("theme", "dark"); // Set dark theme preference
-  } else {
-    themeLink.href = "light.css";
-    localStorage.setItem("theme", "light"); // Set light theme preference
-  }
-
-  // Notify all other pages about the theme change
-  var allPages = document.querySelectorAll('[data-theme="changeable"]');
-  allPages.forEach(function (page) {
-    page.contentWindow.postMessage({ theme: currentTheme }, "*");
-  });
+    var themeLink = document.getElementById("theme-link");
+    // Check the current theme preference in local storage
+    var currentTheme = localStorage.getItem("theme") || "light";
+    // Toggle the theme by changing the href attribute of the link element
+    if (currentTheme === "light") {
+        themeLink.href = "style.css";
+        localStorage.setItem("theme", "dark"); // Set dark theme preference
+    }
+    else {
+        themeLink.href = "light.css";
+        localStorage.setItem("theme", "light"); // Set light theme preference
+    }
+    // Notify all other pages about the theme change
+    var allPages = document.querySelectorAll('[data-theme="changeable"]');
+    allPages.forEach(function (page) {
+        if (page instanceof HTMLIFrameElement) {
+            page.contentWindow.postMessage({ theme: currentTheme }, "*");
+        }
+    });
 }
-
 var themeToggleBtn = document.getElementById("theme-toggle");
-if (themeToggleBtn) themeToggleBtn.addEventListener("click", toggleTheme);
-
+if (themeToggleBtn)
+    themeToggleBtn.addEventListener("click", toggleTheme);
 window.addEventListener("load", function () {
-  var themeLink = document.getElementById("theme-link");
-  var body = document.body;
-  var currentTheme = localStorage.getItem("theme") || "light";
-
-  if (currentTheme === "light") {
-    themeLink.href = "light.css";
-    body.classList.add("light-theme");
-  } else {
-    themeLink.href = "style.css";
-  }
+    var themeLink = document.getElementById("theme-link");
+    var body = document.body;
+    var currentTheme = localStorage.getItem("theme") || "light";
+    if (currentTheme === "light") {
+        themeLink.href = "light.css";
+        body.classList.add("light-theme");
+    }
+    else {
+        themeLink.href = "style.css";
+    }
 });
--- function.ts
+++ function.ts
@@ -1,21 +1,42 @@
-// Define a boolean variable to track the current theme
-let islight: boolean = true;
-
 // Function to toggle the theme
 function toggleTheme() {
   const themeLink = document.getElementById("theme-link") as HTMLLinkElement;
 
+  // Check the current theme preference in local storage
+  const currentTheme = localStorage.getItem("theme") || "light";
+
   // Toggle the theme by changing the href attribute of the link element
-  if (islight) {
+  if (currentTheme === "light") {
     themeLink.href = "style.css";
+    localStorage.setItem("theme", "dark"); // Set dark theme preference
   } else {
     themeLink.href = "light.css";
+    localStorage.setItem("theme", "light"); // Set light theme preference
   }
 
-  // Update the theme toggle state
-  islight = !islight;
+  // Notify all other pages about the theme change
+  const allPages = document.querySelectorAll('[data-theme="changeable"]');
+  allPages.forEach((page) => {
+    if (page instanceof HTMLIFrameElement) {
+      page.contentWindow.postMessage({ theme: currentTheme }, "*");
+    }
+  });
 }
 
-// Add a click event listener to the button
-const themeToggleBtn = document.getElementById("theme-toggle");
+const themeToggleBtn = document.getElementById(
+  "theme-toggle"
+) as HTMLButtonElement | null;
 if (themeToggleBtn) themeToggleBtn.addEventListener("click", toggleTheme);
+
+window.addEventListener("load", () => {
+  const themeLink = document.getElementById("theme-link") as HTMLLinkElement;
+  const body = document.body;
+  const currentTheme = localStorage.getItem("theme") || "light";
+
+  if (currentTheme === "light") {
+    themeLink.href = "light.css";
+    body.classList.add("light-theme");
+  } else {
+    themeLink.href = "style.css";
+  }
+});
